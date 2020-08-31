ecf3ab1b2ee09bd93e82a2d7b590ec8e66660796
JadaMilligan <70172897+JadaMilligan@users.noreply.github.com>
2020-08-31 16:54:02 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.lang.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int t;
+        t = 95;
+        System.out.println( "Test Score initialized is: " + t);
+        float p;
+        p = 10.35f;
+        System.out.println(" Price tag: " + p);
+        String user = "Bob Dylan";
+        System.out.println("Username: " + user);
+
+    }
+}
